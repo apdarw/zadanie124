60ac1d01b716b7bf784fdafca562efcd66d261a4
Mateusz Wójcik <matis140599@onet.pl>
2021-01-28 18:17:11 +0100

    Dodanie obliczanie pola trójkąta

--- pole_prostokata.c
+++ pole_prostokata.c
@@ -1,27 +1,44 @@
 #include <stdio.h>
 
 float pole_prostokata(float a, float b);
+float pole_trojkata(float a, float b, float c);
 
 int main() {
 
-    float dlugosc, szerokosc, pole;
+    float dl_boku_1, dl_boku_2, dl_boku_3, pole_pros, pole_troj;
 
     printf("Podaj dlugosc pierwszego boku: ");
-    scanf ("%f", &dlugosc);
+    scanf ("%f", &dl_boku_1);
 
     printf("\nPodaj dlugosc drugiego boku: ");
-    scanf ("%f", &szerokosc);
+    scanf ("%f", &dl_boku_2);
 
-	pole = pole_prostokata(dlugosc, szerokosc);
-    printf ("\nPole prostokata wynosi: %.2f", pole);
+    printf("\nPodaj dlugosc trzeciego boku: ");
+    scanf ("%f", &dl_boku_3);
+
+    pole_pros = pole_prostokata(dl_boku_1, dl_boku_2);
+    printf ("\nPole prostokata wynosi: %.2f", pole_pros);
+    printf("\n");
+
+	pole_troj = pole_trojkata(dl_boku_1, dl_boku_2, dl_boku_3);
+    printf ("\nPole trojkata wynosi: %.2f", pole_troj);
     printf("\n");
 
 return 0;
     }
+
 float pole_prostokata(float a, float b){
 	float y;
 
 	y = a*b;
+
+	return(y);
+}
+float pole_trojkata(float a, float b, float c){
+	float y, p;
+
+    p = (a + b + c) / (float)2;
+	y = sqrt(p*(p-a)*(p-b)*(p-c));
 
 	return(y);
 }
